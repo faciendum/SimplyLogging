6d8384946c01d18eeedba5974e4408c1c2579e05
faciendum <henning@familiekoetz.info>
2020-10-07 16:22:12 +0200

    update

---
+++ SimplyLogging/Core/Handlers/ConsoleLogging.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SimplyLogging.Core.Interfaces;
+using SimplyLogging.Core.Models;
+using SimplyLogging.Core.Styles;
+
+namespace SimplyLogging.Core.Handlers
+{
+   public class ConsoleLogging : ILoggingObject
+    {
+        private readonly ILoggingStyle _logStyle;
+
+        public ConsoleLogging() : this(new DefaultStyle()) { }
+
+        public ConsoleLogging(ILoggingStyle logStyle)
+        {
+            _logStyle = logStyle;
+        }
+
+        public void Write(LogMessage logMessage)
+        {
+           Console.WriteLine(_logStyle.ApplyStyle(logMessage));
+        }
+    }
+}
